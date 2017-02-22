82e6ecdcd52a057997c4998887763ed209bc01f4
blackknight75 <devbox4714@gmail.com>
2017-02-21 19:10:08 -0700

    add index page and can see all items as visitor

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  # root '/items', to: 'items#index', as: 'root'
 
+  get '/items', to: 'items#index', as: 'root'
+  resources :items, only: [:new]
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
