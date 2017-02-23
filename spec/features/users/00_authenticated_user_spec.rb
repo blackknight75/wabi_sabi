6cd28413820a932e7384f0be084646aaa781a8c2
Spencer Carter <scmountain@gmail.com>
2017-02-22 23:08:35 -0700

    Continue to add to test SQUASH

--- spec/features/users/00_authenticated_user_spec.rb
+++ spec/features/users/00_authenticated_user_spec.rb
@@ -5,24 +5,33 @@ RSpec.feature "as a visitor" do
     visit root_path
     click_on "Login"
     expect(current_path).to eq('/login')
-    fill_in 'user[username]'
-    fill_in 'user[password]'
-    fill_in 'user[password_confirmation]'
+    fill_in 'user[username]', with: "Billy"
+    fill_in 'user[password]', with: "pass"
+    fill_in 'user[password_confirmation]', with: "pass"
 
     click_on "Login"
-    expect(current_path).to eq()
+    expect(current_path).to eq("/dashboard")
+  end
 
+  scenario "visitor can create an account" do
+    visit root_path
+    click_on "Login"
+    expect(current_path).to eq('/login')
+    click_on "Create Account"
+    expect(current_path).to eq('/signup')
 
+    fill_in "login[username]", with "Sally"
+    fill_in "login[email]", with "Sally@email.com"
+    fill_in "login[address]", with "123 2nd St"
+    fill_in "login[password]", with "pass"
+    fill_in "login[password_confimation]", with "pass"
+
+    click_on "Save New Account"
+    expect(current_path).to eq("/dashboard")
+    expect(page).to have_content
   end
 end
-# As a visitor
-# When I visit "/"
-# Then I should see a link for "Login"
-# And when I click "Login"
-# And I should be on the "/login" page
-# I should see a place to insert my credentials to login
-# And I should see a link to "Create Account"
-#
+
 # As a visitor
 # When I visit "/login
 # And when I click link "Create Account"
