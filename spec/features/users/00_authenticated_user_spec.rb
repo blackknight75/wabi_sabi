96ab6f80203955d6c622bfbfdeb5213a6d78d93a
Spencer Carter <scmountain@gmail.com>
2017-02-23 15:26:01 -0700

    Add test for user authentication/creation
    adds only the feature test based of of the user story. This test will drive the creation of a login page and
    the creation of a create account. Also will drive the creation for the user table.

--- spec/features/users/00_authenticated_user_spec.rb
+++ spec/features/users/00_authenticated_user_spec.rb
@@ -28,17 +28,10 @@ RSpec.feature "as a visitor" do
 
     click_on "Save New Account"
     expect(current_path).to eq("/dashboard")
-    expect(page).to have_content
+    expect(page).to have_content("Logged in as Sally")
+    expect(page).to have_content("Sally@email.com")
+    expect(page).to have_content("123 2nd St")
+    expect(page).to_not have_content("Login")
+    expect(page).to have_content("Logout")
   end
 end
-
-# As a visitor
-# When I visit "/login
-# And when I click link "Create Account"
-# And I fill in my desired credentials
-# And I submit my information
-# Then my current page should be "/dashboard"
-# And I should see a message in the navbar that says "Logged in as SOME_USER"
-# And I should see my profile information
-# And I should not see a link for "Login"
-# And I should see a link for "Logout"
