bf6813d37f3f45e92bc9207dbae0378e01affd6c
Spencer Carter <scmountain@gmail.com>
2017-02-23 21:18:02 -0700

    add login and logout capabilities

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_cart
   helper_method :current_user
-  
+
   def set_cart
     @cart = Cart.new(session[:cart])
   end
 
   def current_user
-    @user ||= User.find(sesion[:user_id]) if session[:user_id]
+    @user ||= User.find(session[:user_id]) if session[:user_id]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,12 @@ Rails.application.routes.draw do
   get '/cart', to: 'carts#show', as: "cart"
   post '/cart', to: 'carts#create'
   delete '/cart', to: 'carts#destroy'
+  get '/login', to: 'sessions#new', as: "login"
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
 
   resources :items, only: [:new, :show]
-  resources :users, only: [:new, :show]
+  resources :users, only: [:new, :show, :create]
 
   get ":slug" => "categories#show", :as => "category"
 end
