bba3f8c712b61a50c112e5e07b7226a7c7729035
Spencer Carter <scmountain@gmail.com>
2017-02-26 00:00:37 -0700

    orders page is populated

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170225050040) do
-
+ActiveRecord::Schema.define(version: 20170226062447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +39,11 @@ ActiveRecord::Schema.define(version: 20170225050040) do
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.boolean  "retired",     default: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
---
+++ spec/features/order_items/authenticated_user_can_checkout_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe ' when a user visits their cart' do
+  scenario ' they click on checkout and order is placed' do
+    user = User.create(username: "sally", email: "sally@email.com", password: "pass")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    item = Item.create(title: "Matcha",
+                       description: "Green Tea",
+                       price: 100)
+    Item.create(title: "Stuff",
+                       description: "Green Stuff",
+                       price: 100)
+
+    visit root_path
+      within all('.item-card').first do
+        click_on "Add to Cart"
+    end
+      within('.item-card:nth-child(2)') do
+        click_on "Add to Cart"
+    end
+      within('.item-card:nth-child(1)') do
+        click_on "Add to Cart"
+    end
+
+    click_on "View Cart"
+
+    expect(current_path).to eq cart_path
+
+    click_on "checkout"
+    expect(current_path).to eq new_order_path
+    expect(page).to have_content("Matcha")
+    expect(page).to have_content("Stuff")
+
+    click_on "Submit"
+
+    expect(current_path).to eq root_path
+    expect(page).to have_content("Order was successfully placed")
+  end
+end
