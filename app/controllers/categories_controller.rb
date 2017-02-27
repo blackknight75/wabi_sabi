15699bc7a7cc9ce57aad64384ce8004447cc97bf
Spencer Carter <scmountain@gmail.com>
2017-02-27 11:42:11 -0700

    add categories index

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,9 @@
 class CategoriesController < ApplicationController
 
+  def index
+    @categories = Category.all
+  end
+
   def show
     @category = Category.find_by_slug(params[:slug])
     @items = @category.items unless @category.nil?
