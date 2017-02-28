7c32614267a4e2cc47806337e3a2df21c9838820
Spencer Carter <scmountain@gmail.com>
2017-02-28 13:56:56 -0700

    Update dashboard_controller.rb
    deleted pry

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,7 +2,6 @@ class Admin::DashboardController < ApplicationController
   before_action :admin_gate
 
   def index
-    # binding.pry
     @orders = Order.all
     @ordered_orders = Order.where(order_status: "Ordered")
   end
