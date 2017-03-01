5b032ca75128f4f1cb41b7fcdd4b5ce7e4ae6897
Spencer Carter <scmountain@gmail.com>
2017-03-01 15:22:16 -0700

    pre pull

---
+++ spec/features/admins/admin_can_edit_item_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.feature "as an admin" do
+  scenario "I can edit exisiting items" do
+    admin = User.create(first_name: "Brandon",
+                      username: "B1",
+                      password: "password",
+                      role: 0)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    item1 = Item.create(title: "Matcha",
+                        description: "Green Tea",
+                        price: 100
+                        )
+    visit'admin/items'
+    click_on "Edit"
+
+    fill_in "item[title]", with: "Bubbles"
+    fill_in "item[status]", with: "Paid"
+    fill_in "item[image]", with: "http://dreamatico.com/data_images/bubble/bubble-7.jpg"
+    fill_in "item[description]", with: "semi-permeable membrane"
+    expect(current_path).to eq.("/admin/items/#{item1.id}/edit")
+  end
+end
